5b49e12e1df0693fcfcebf6ae297a3f2240668b2
flaat <giorgi.flavio.at@gmail.com>
2020-02-29 19:49:54 +0100

    bugs fixed

--- crawler.py
+++ crawler.py
@@ -10,90 +10,93 @@ import requests
 from queue import Queue
 import play_scraper as pl
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import regex as re
 import csv
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from multiprocessing import Process
 
-###variable###
-CSV_APP_PRIVACY_NAME = "apps_privacy_dataset.csv"
-TO_CSV_NUMBER = 10
-QUEUE_MAX_SIZE = 10000
-STARTING_POINT = "https://play.google.com/store/apps/category/GAME"
-visited_links = set()
-apps_info_dataset = []
-apps_privacy_dataset = []
-driver = webdriver.Firefox()
+list = ["https://play.google.com/store/apps/details?id=com.gramgames.mergedragons&hl=en"]
 
+class crawler():
 
-###get_store_infos###
-def get_store_infos(id,str):
-    global dataset
-    global driver
-    temp_page = driver.get(str)
-    privacy_page = driver.find_element_by_xpath("//a[@jsname='Hly47e']")
-    privacy_page.click()
-    privacy_elements = driver.find_elements_by_xpath("//li[@class='BCMWSd']")
-    regular_expression = re.compile("(<li class=\"BCMWSd\"><span>|</span></li>)")
-    privacy_elements_list = [re.sub(regular_expression,"",element.get_attribute("outerHTML")) for element in privacy_elements]
-    info = pl.details(id)
-    apps_privacy_dataset.append([info.get('app_id'),privacy_elements_list,info.get('price'),info.get("iap"),info.get("iap_range")])
+    def __init__(self,csv_name):
+        ###variable###
+        self.CSV_APP_PRIVACY_NAME = csv_name
+        self.TO_CSV_NUMBER = 10
+        self.QUEUE_MAX_SIZE = 10000
+        self.visited_links = set()
+        self.apps_privacy_dataset = []
+        self.driver = webdriver.Firefox()
+        self.restart_links_number = 0
+        self.MAX_LINKS = 150
 
-
-
-
-
-###get_links###
-def get_links(tree):
-    global visited_links
-    return_list = []
-    # This will get the anchor tags <a href...>
-    refs = tree.xpath("//a")
-    # Get the url from the ref
-    links = [link.get('href', '') for link in refs]
-
-    for l in links:
-        str = "https://play.google.com"+l
-        if  "/apps/details?id=" in str and str not in visited_links:
-            if len(apps_privacy_dataset)%TO_CSV_NUMBER == 0:
-                with open(CSV_APP_PRIVACY_NAME, "w") as f:
-                    writer = csv.writer(f)
-                    writer.writerows(apps_privacy_dataset)
-            return_list.append(str)
-            visited_links.add(str)
-            get_store_infos(str.split("?id=")[1],str)
-        str = ""
-    return return_list
+    ###get_store_infos###
+    def get_store_infos(self,id,str):
+        temp_page = self.driver.get(str)
+        privacy_page = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, "//a[@jsname='Hly47e']")))
+        privacy_page.click()
+        privacy_elements = self.driver.find_elements_by_xpath("//li[@class='BCMWSd']")
+        regular_expression = re.compile("(<li class=\"BCMWSd\"><span>|</span></li>)")
+        privacy_elements_list = [re.sub(regular_expression,"",element.get_attribute("outerHTML")) for element in privacy_elements]
+        info = pl.details(id)
+        self.apps_privacy_dataset.append([info.get('app_id'),privacy_elements_list,info.get('price'),info.get("iap"),info.get("iap_range")])
 
 
-###explore###
-def explore(init):
-    global dataset
-    # Set explicit HTMLParser
-    parser = etree.HTMLParser()
-
-    link_queue = Queue(maxsize=QUEUE_MAX_SIZE)
-
-    link_queue.put(init)
-
-    while not link_queue.empty():
 
 
-        print("QUEUE_SIZE :", link_queue.qsize())
+    ###get_links###
+    def get_links(self,tree):
+        return_list = []
+        # This will get the anchor tags <a href...>
+        refs = tree.xpath("//a")
+        # Get the url from the ref
+        links = [link.get('href', '') for link in refs]
+        for l in links:
 
-        page_link = link_queue.get()
+            str = "https://play.google.com"+l
+            if  "/apps/details?id=" in str and str not in self.visited_links:
+                if len(self.apps_privacy_dataset)%self.TO_CSV_NUMBER == 0:
+                    with open(self.CSV_APP_PRIVACY_NAME, "a") as f:
+                        writer = csv.writer(f)
+                        writer.writerows(self.apps_privacy_dataset)
+                        self.apps_privacy_dataset = []
+                return_list.append(str)
+                self.visited_links.add(str)
+                self.restart_links_number +=1
+                self.get_store_infos(str.split("?id=")[1],str)
+            str = ""
+        return return_list
 
-        print("Exploring: ",page_link)
 
-        page = requests.get(page_link)
+    ###explore###
+    def explore(self,init):
+        with open(self.CSV_APP_PRIVACY_NAME, "w") as f:
+            writer = csv.writer(f)
+        # Set explicit HTMLParser
+        parser = etree.HTMLParser()
+        link_queue = Queue(maxsize=self.QUEUE_MAX_SIZE)
+        link_queue.put(init)
 
-        html = page.content.decode("utf-8")
+        while not link_queue.empty():
+            if (self.restart_links_number>self.MAX_LINKS) ==0:
+                    self.driver.quit()
+                    self.driver = webdriver.Firefox()
+                    self.restart_links_number = 0
+            print("QUEUE_SIZE :", link_queue.qsize())
+            page_link = link_queue.get()
+            print("Exploring: ",page_link)
+            page = requests.get(page_link)
+            html = page.content.decode("utf-8")
+            tree = etree.parse(StringIO(html), parser=parser)
+            [link_queue.put(l) for l in self.get_links(tree)]
 
-        tree = etree.parse(StringIO(html), parser=parser)
+i = 0
+for l in list:
+    # Example call
 
-        links = get_links(tree)
-
-        [link_queue.put(l) for l in links]
-
-
-# Example call
-explore(STARTING_POINT)
+    p = Process(target=crawler(str(i)+".csv").explore, args=(l,))
+    p.start()
+    i+=1
